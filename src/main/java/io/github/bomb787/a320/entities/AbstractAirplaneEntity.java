6708feb11f611b21611f9df692d4711848d1844c
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2024-06-25 17:00:15 -0400

    🔨 More misc. additions

--- src/main/java/io/github/bomb787/a320/entities/AbstractAirplaneEntity.java
+++ src/main/java/io/github/bomb787/a320/entities/AbstractAirplaneEntity.java
@@ -5,6 +5,10 @@ import net.minecraft.world.World;
 
 public abstract class AbstractAirplaneEntity extends AbstractFlyingVehicleEntity {
 
+    /**
+     * <a href="https://eaglepubs.erau.edu/introductiontoaerospaceflightvehicles/chapter/finite-wing-characteristics/">Useful ERAU site</a>
+     * <a href="https://www.grc.nasa.gov/www/k-12/VirtualAero/BottleRocket/airplane/induced.html">Useful NASA site</a>
+     */
     public AbstractAirplaneEntity(EntityType<?> type, World world) {
         super(type, world);
     }
--- src/main/java/io/github/bomb787/a320/utils/PhysicsUtils.java
+++ src/main/java/io/github/bomb787/a320/utils/PhysicsUtils.java
@@ -1,9 +1,18 @@
 package io.github.bomb787.a320.utils;
 
 import io.github.bomb787.a320.entities.AbstractFlyingVehicleEntity;
+import net.minecraft.util.Util;
 
 public class PhysicsUtils {
 
-    public static void getLift(AbstractFlyingVehicleEntity entity) {
+    //Generic coefficient of lift based off of NACA23012
+    public static final float[] DEFAULT_CL = Util.make(new float[65536], default_cl -> {
+        for(int i = 0; i < default_cl.length; i++) {
+            default_cl[i] = i;
+        }
+    });
+
+    public static void lift(AbstractFlyingVehicleEntity entity) {
+
     }
 }
